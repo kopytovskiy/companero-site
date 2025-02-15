d1a3f4fd47a720dfdca40e1406a6524efcb29ce6
Maksym Kopytovskyi <m.kopytovskiy@gmail.com>
2025-02-15 12:24:40 +0000

    Update images with webp format

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,10 +26,10 @@ Author: GrayGrids
         var logo = document.querySelector('.navbar-brand img')
         if (window.pageYOffset > sticky) {
           header_navbar.classList.add("sticky");
-          logo.src = 'assets/images/logo/logo.png';
+          logo.src = 'assets/images/logo/logo.webp';
         } else {
           header_navbar.classList.remove("sticky");
-          logo.src = 'assets/images/logo/white-logo.png';
+          logo.src = 'assets/images/logo/white-logo.webp';
         }
 
         // show or hide the back-top-top button
